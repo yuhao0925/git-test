82fccbcc5fff102443b7b6b913b39e81765c0371
yuhao0925 <53131897+yuhao0925@users.noreply.github.com>
2021-07-04 15:03:39 +0800

    Update GitTest.java
    git pull

--- src/main/java/com/yuhao/git/GitTest.java
+++ src/main/java/com/yuhao/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master-test!!!!!");
         System.out.println("hot-fix!!!!!!");
         System.out.println("git push !!!!!!");
+        System.out.println("git pull !!!!!!");
 
     }
 }
